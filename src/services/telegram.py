fa76f053094434e9743b99178e843d9d2e90d108
DevOps <dev@tasker.internal>
2026-01-13 22:32:18 +0300

    112в

--- src/services/telegram.py
+++ src/services/telegram.py
@@ -3,8 +3,8 @@ import logging
 
 class TelegramService:
     def __init__(self):
-        self.token = "YOUR_TOKEN"
-        self.chat_id = "YOUR_CHAT_ID"
+        self.token = "6248133659:AAH5MdnQbbtfhRj3MbRzSGmmKFg73xhN08Q"
+        self.chat_id = "365401498"
     
     def send_notification(self, message: str):
         if "YOUR_" in self.token:
